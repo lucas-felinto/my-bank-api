ac329c022ed4a1968fe789d42d0ac08e05cec843
lucas-felinto <lucasfepinto@gmail.com>
2020-06-14 22:13:31 -0300

    feat: add accounts

--- index.js
+++ index.js
@@ -10,11 +10,49 @@ app.get("/", (req, res) => {
 });
 
 app.post("/account", (req, res) => {
-  fs.writeFile("accounts.json", JSON.stringify(req.body, null, 2), (err) => {
-    console.log(err);
+  let account = req.body;
+
+  fs.readFile("accounts.json", "utf8", (err, data) => {
+    if (!err) {
+      try {
+        let json = JSON.parse(data);
+        json = {
+          id: json.nextId++,
+          ...account,
+        };
+        json.accounts.push(account);
+        fs.writeFile(
+          "accounts.json",
+          JSON.stringify(account, null, 2),
+          (err) => {
+            if (err) return res.send(err);
+            res.send("post account");
+          }
+        );
+      } catch (err) {
+        res.status(400).send({ error: err.message });
+      }
+    }
   });
 });
 
 app.listen(3000, () => {
+  try {
+    fs.readFile("accounts.json", "utf8", (err) => {
+      if (err) {
+        const initialJson = {
+          nextId: 1,
+          accounts: [],
+        };
+        fs.writeFile(
+          "accounts.json",
+          JSON.stringify(initialJson, null, 2),
+          (err) => console.log(err)
+        );
+      }
+    });
+  } catch (err) {
+    console.log(err);
+  }
   console.log("API Started on Port 3000");
 });
